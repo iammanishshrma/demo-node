a277c386dc78a132386c90daf8c44bb887307cd8
Manish <iammanishshrma@gmail.com>
2024-05-24 15:38:18 +0530

    api error/response util added

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+// Used to allow cross origins
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
+    })
+);
+
+app.use(
+    express.json({
+        limit: "16kb",
+    })
+);
+app.use(
+    express.urlencoded({
+        extended: true,
+        limit: "16kb",
+    })
+);
+//To store files on local server in /public
+app.use(express.static("public"));
+
+app.use(cookieParser());
+
+export { app };
